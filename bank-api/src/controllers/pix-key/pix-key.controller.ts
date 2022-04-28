6a7d5d95c698a24036707d2124fe473a04843f7a
Rodrigo Engelberg <rodrigoengelberg@gmail.com>
2022-04-28 03:10:58 -0300

    Adição do PixKeyController

--- bank-api/src/controllers/pix-key/pix-key.controller.ts
+++ bank-api/src/controllers/pix-key/pix-key.controller.ts
@@ -1,5 +1,15 @@
-import { Controller, Get, Param, ParseUUIDPipe, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  ValidationPipe,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { PixKeyDto } from 'src/dto/pix-key.dto';
+import { BankAccount } from 'src/models/bank-account.model';
 import { PixKey } from 'src/models/pix-key.model';
 import { Repository } from 'typeorm';
 
@@ -8,6 +18,8 @@ export class PixKeyController {
   constructor(
     @InjectRepository(PixKey)
     private pixKeyRepo: Repository<PixKey>,
+    @InjectRepository(BankAccount)
+    private bankAccountRepo: Repository<BankAccount>,
   ) {}
   @Get()
   index(
@@ -25,8 +37,15 @@ export class PixKeyController {
   }
 
   @Post()
-  store(
+  async store(
     @Param('bankAccountId', new ParseUUIDPipe({ version: '4' }))
     bankAccoutId: string,
-  ) {}
+    @Body(new ValidationPipe({ errorHttpStatusCode: 422 }))
+    body: PixKeyDto,
+  ) {
+    await this.bankAccountRepo.findOneOrFail(bankAccoutId);
+  }
+
+  @Get('exists')
+  exists() {}
 }
